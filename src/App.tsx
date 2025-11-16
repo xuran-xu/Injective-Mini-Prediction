ce566f94ce0a7dc03738d17190645fe989c1d3a1
xuran <xuran.xu@connect.polyu.hk>
2025-11-16 20:46:37 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Home } from 'lucide-react'
+
+function App() {
+  return (
+    <div>
+      <h1>Injective-Mini-Prediction</h1>
+      <Home size={24} />
+      <p>Coming Soon!</p>
+    </div>
+  )
+}
+
+export default App
